fd0e7515ba6b8cab35a20d758dfad224a07cc994
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-09-12 19:29:32 -0300

    Update 05-zerinho-ou-um.c

--- lista00-revisao-c/05-zerinho-ou-um.c
+++ lista00-revisao-c/05-zerinho-ou-um.c
@@ -1,5 +1,5 @@
 /*
-Zerinho ou Um
+ZERINHO OU UM
 
 O jogo conhecido como Zerinho ou Um e utilizado para determinar um ganhador entre tres jogadores. Cada jogador escolhe o valor zero ou um; apos um comando (geralmente um dos competidores anuncia em voz alta Zerinho ou... Um!), todos os participantes mostram o valor escolhido, utilizando uma das maos: se o valor escolhido e um, o competidor mostra o dedo indicador estendido; se o valor escolhido e zero, mostra a mao com todos os dedos fechados. O ganhador e aquele que tiver escolhido um valor diferente de todos os outros; se nao ha um jogador com valor diferente de todos os outros (por exemplo todos os jogadores escolhem zero), nao ha ganhador. Escreva um programa em C que receba os valores que os tres jogadores colocaram e determine quem venceu a brincadeira. PS: Qual a brincadeira favorita do computador? Zerinho ou um!
 
