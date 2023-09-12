34bdf7d4f58c43fa0e1315e122b5213bfea8ee38
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-09-12 19:30:47 -0300

    Update 03-distancia-de-manhattan.c

--- lista00-revisao-c/03-distancia-de-manhattan.c
+++ lista00-revisao-c/03-distancia-de-manhattan.c
@@ -1,5 +1,5 @@
 /*
-Distância de Manhattan
+DISTANCIA DE MANHATTAN
 Maria é uma moradora de Nlogópolis, uma cidade na Nlogônia que tem uma característica muito interessante: todas as ruas da cidade ou são orientadas no sentido norte-sul ou são orientadas no sentido leste-oeste. Isso significa que, dadas duas ruas, ou elas são paralelas ou elas são perpendiculares entre si. Todas as ruas da cidade são de mão dupla e é possível seguir em qualquer direção em um cruzamento. Agora Maria está atrasada para uma reunião e precisa de sua ajuda. Dadas as coordenadas iniciais de Maria e da reunião, determine o número mínimo de cruzamentos que Maria deve atravessar para chegar ao seu destino. Esse número inclui o cruzamento onde ocorrerá a reunião mas não inclui a posição inicial de Maria.
 
 Entrada
