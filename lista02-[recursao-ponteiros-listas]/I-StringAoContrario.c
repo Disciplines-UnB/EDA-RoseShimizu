b7e626a4d95347df99672347734a641ea06d8d13
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-11-04 00:00:59 -0300

    Accepted,100p
    String ao contrário
    Faça um programa que leia uma string e a imprima ao contrário.
    
    Atenção: Você não pode usar laços na sua solução.
    
    Entrada
    A entrada é composta por uma única linha contendo uma string de, no máximo, 500 caracteres, contendo letras (maiúsculas e minúsculas, não acentuadas) e números.
    
    Saída
    A saída deve conter uma única linha com a string impressa ao contrário.
    
    Exemplo de Entrada 1
    amor
    Exemplo de Saída 1
    roma
    
    Exemplo de Entrada 2
    paralelepipedo
    Exemplo de Saída 2
    odepipelelarap
    
    
    Author: John L. Gardenghi
    
    
    Co-authored-by: Nathalia Assis <nathaliaassisp@gmail.com>

---
+++ lista02-[recursao-ponteiros-listas]/I-StringAoContrario.c
@@ -0,0 +1,43 @@
+/*
+String ao contrário
+Faça um programa que leia uma string e a imprima ao contrário.
+
+Atenção: Você não pode usar laços na sua solução.
+
+Entrada
+A entrada é composta por uma única linha contendo uma string de, no máximo, 500 caracteres, contendo letras (maiúsculas e minúsculas, não acentuadas) e números.
+
+Saída
+A saída deve conter uma única linha com a string impressa ao contrário.
+
+Exemplo de Entrada 1
+amor
+Exemplo de Saída 1
+roma
+
+Exemplo de Entrada 2
+paralelepipedo
+Exemplo de Saída 2
+odepipelelarap
+
+
+Author: John L. Gardenghi
+*/
+
+
+
+#include <stdio.h>
+
+void inverterString(char *string) {
+    if (*string == '\0') {return;}
+    inverterString(string + 1);
+    printf("%c", *string);
+}
+
+int main() {
+    char string[501];
+    scanf("%s", string);  
+    inverterString(string);
+    printf("\n");
+    return 0;
+}
